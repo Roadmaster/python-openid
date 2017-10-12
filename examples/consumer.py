9616e25870adaa7eff5f02af5f82ad6a1a96a50a
Daniel Manrique <roadmr@ubuntu.com>
2017-10-12 16:21:24 -0400

    Send a response code so the http response is properly formatted

--- examples/consumer.py
+++ examples/consumer.py
@@ -207,7 +207,7 @@ class OpenIDRequestHandler(BaseHTTPRequestHandler):
                         trust_root, return_to,
                         form_tag_attrs={'id':'openid_message'},
                         immediate=immediate)
-
+                    self.send_response(200)
                     self.wfile.write(form_html)
 
     def requestRegistrationData(self, request):
